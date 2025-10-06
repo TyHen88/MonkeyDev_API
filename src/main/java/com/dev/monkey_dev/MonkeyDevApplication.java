ddcb317ba1ee7f12cfd9b29756e2b803d1ecf8f5
Hen Ty <henty@Ti-MacBook-Pro.local>
2025-10-06 11:33:08 +0700

    Initial commit

---
+++ src/main/java/com/dev/monkey_dev/MonkeyDevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.monkey_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/monkey_dev/MonkeyDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.monkey_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
